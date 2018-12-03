20042527d6b26c113e624e29a0b86e69aa5e6597
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2018-12-03 03:05:56 -0800

    starting to set up dis spa

---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+        <div>
+            <p>yes</p>
+        </div>
+    </main>
+
+</template>
+<script>
+export default {
+    name:"App",
+    data(){
+
+    }
+}
+</script>
