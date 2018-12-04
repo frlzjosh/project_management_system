bb4f7ef6bd4f4a67acd7a1cd0e46bf862387ea98
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2018-12-03 22:05:38 -0800

    where our sidebar will be

---
+++ resources/js/components/sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="sidebar">
+    <div clas="sidebar__home-icon">
+    </div>
+    <ul class="sidebar__list">
+        <li class="sidebar__list-item">Deliverables</li>
+        <li class="sidebar__list-item">Tasks</li>
+        <li class="sidebar__list-item">Issues</li>
+        <li class="sidebar__list-item">Changes</li>
+        <li class="sidebar__list-item">References</li>
+        <li class="sidebar__list-item">Resources</li>
+        <li class="sidebar__list-item">Action Items</li>
+        <li class="sidebar__list-item">Requirements</li>
+        <li class="sidebar__list-item">Decisions</li>
+        <li class="sidebar__list-item">Risks</li>
+    </ul>
+</div>
+</template>
+<script>
+</script>
+
