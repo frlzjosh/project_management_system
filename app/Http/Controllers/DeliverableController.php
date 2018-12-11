9419d26788b32c79424b369ec129ef962233b654
ekno27 <edgkano@gmail.com>
2018-12-10 18:29:00 -0800

    get route to guve deliverable have been created

--- app/Http/Controllers/DeliverableController.php
+++ app/Http/Controllers/DeliverableController.php
@@ -85,4 +85,8 @@ class DeliverableController extends Controller
     public function testing(){
         return 'Small but significant process';
     }
+    public function getAll() {
+        $allDeliverables = Deliverable::all();
+        return $allDeliverables;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,10 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+//deliverables
 Route::get('deliverable/hello', 'DeliverableController@testing');
+Route::get('deliverable/all', 'DeliverableController@getAll');
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
